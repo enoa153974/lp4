499cb7d9c7f99b5f939eca8d68b58077313b3907
enoa153974 <aoi.153974@gmail.com>
2025-06-03 09:08:16 +0900

    初回コミット

---
+++ siteData.mjs
@@ -0,0 +1,23 @@
+export default {
+    home: {
+        title: 'サイトタイトル',
+        description: 'サイトタイトルの説明文です',
+        ogpImage: '/assets/images/ogp_default.png',
+        type: 'website',
+        path: '/'
+    },
+    example: {
+        title: '下層サンプルページ',
+        description: '下層サンプルページの説明文です',
+        ogpImage: '/assets/images/ogp_default.png',
+        type: 'article',
+        path: '/example.html'
+    },
+    posts: {
+        title: '投稿一覧',
+        description: '投稿一覧ページの説明文です',
+        ogpImage: '/assets/images/ogp_default.png',
+        type: 'article',
+        path: '/posts/'
+    }
+};
