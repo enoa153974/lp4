8104fd2feef3ec7d1adb40d3e848ee9d7e9ce888
enoa153974 <aoi.153974@gmail.com>
2025-06-03 12:23:04 +0900

    全体の枠組みを作成（作業途中）

--- public/assets/scripts/ui/main.js
+++ public/assets/scripts/ui/main.js
@@ -3,13 +3,11 @@
 //以下の構文で他ファイルに定義された関数の処理を使いまわすことができる
 import { initSlider } from './slider.js';
 import { accordion } from './accordion.js';
-import { initTabs } from './tabs.js';
 
 //ui.jsからハンバーガーメニューのとタブメニューの関数をインポートする
 window.addEventListener('DOMContentLoaded', () => {
     // HTML読み込みが終わったあとに実行される処理
     initSlider();
     accordion();
-    initTabs();
     console.log('jsファイルの読み込みが完了しました！');
 });
