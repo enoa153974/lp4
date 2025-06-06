888c291fbdea2a151cf6810ab46d76b90a68e4cf
enoa153974 <aoi.153974@gmail.com>
2025-06-06 11:39:25 +0900

    faqページ作成

--- public/assets/scripts/ui/main.js
+++ public/assets/scripts/ui/main.js
@@ -2,12 +2,12 @@
 
 //以下の構文で他ファイルに定義された関数の処理を使いまわすことができる
 import { initSlider } from './slider.js';
-import { accordion } from './accordion.js';
+import { faqAccordion } from './faqAccordion.js';
 
 //ui.jsからハンバーガーメニューのとタブメニューの関数をインポートする
 window.addEventListener('DOMContentLoaded', () => {
     // HTML読み込みが終わったあとに実行される処理
     initSlider();
-    accordion();
+    faqAccordion();
     console.log('jsファイルの読み込みが完了しました！');
 });
