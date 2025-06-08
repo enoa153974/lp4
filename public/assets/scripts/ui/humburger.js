7f1a8b6df458fc34bdf811e78cafbacbe7e2258d
enoa153974 <aoi.153974@gmail.com>
2025-06-08 23:41:26 +0900

    ピクセル確認済み

--- public/assets/scripts/ui/humburger.js
+++ public/assets/scripts/ui/humburger.js
@@ -1,11 +1,13 @@
 export function humburger() {
-$(".openbtn").click(function () {
-    $(this).toggleClass('active');
-    $("#global-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-});
+    $(".openbtn").click(function () {
+        $(this).toggleClass('active');
+        $("#global-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+        $("body").toggleClass('no-scroll');
+    });
 
-$("#global-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#global-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-});
+    $("#global-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+        $("#global-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+        $("body").removeClass('no-scroll');
+    });
 }
